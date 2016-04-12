a077259df521c2001e6a217c399dfa8c8660214b
berto <berto@q-interactiva.com>
2016-04-12 11:21:10 +0200

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -9,17 +9,17 @@ angular.module('ser', ['ionic']).run(function($ionicPlatform) {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
 
-    alert("inicializando: " +window);
+    alert("inicializando: " +window.cordova);
     
 
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      alert("inicializando1: " +window.cordova);
+      //alert("inicializando1: " +window.cordova);
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-        alert("inicializando2: " +window.StatusBar);
+       //alert("inicializando2: " +window.StatusBar);
       // org.apache.cordova.statusbar required
       window.StatusBar.styleDefault();
     }
