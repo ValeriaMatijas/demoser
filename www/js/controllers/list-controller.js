fcfd3eeeea8187f6a9dea9a9c9e9c7121c1231ce
berto <berto@q-interactiva.com>
2016-04-12 14:18:19 +0200

    no message

--- www/js/controllers/list-controller.js
+++ www/js/controllers/list-controller.js
@@ -4,9 +4,9 @@ angular.module('ser').controller('PlaylistCtrl', ['$stateParams', function( $sta
   self.clickBoton = function()
   {
     //alert("click!");
-    //var ID = "LauRozRub";
-    //window.open("fb://profile/" + ID, '_system');
-     window.open("https://www.facebook.com/choicefm1043");
+    var ID = "LauRozRub";
+    window.open("fb://profile/" + ID, '_system');
+    //window.open("https://www.facebook.com/choicefm1043");
 
   };  
 }]);
