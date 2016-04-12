7cbc08864cb74fc818724ad2b708f7330697cb76
berto <berto@q-interactiva.com>
2016-04-12 11:13:06 +0200

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,6 @@
 // 'ser' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'ser.controllers' is found in controllers.js
-var ref = this;
 angular.module('ser', ['ionic']).run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -26,6 +25,6 @@ angular.module('ser', ['ionic']).run(function($ionicPlatform) {
     }
 
     alert("inicializando3: " +window.cordova );
-    ref.initPushwoosh();
+    initPushwoosh();
   });
 });
