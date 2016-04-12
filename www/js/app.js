91b7cb8466855847d6fd4e9e5f6f625c020081ee
berto <berto@q-interactiva.com>
2016-04-12 10:55:00 +0200

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -8,6 +8,8 @@ angular.module('ser', ['ionic']).run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    alert("inicializando: " +window.cordova );
+
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
