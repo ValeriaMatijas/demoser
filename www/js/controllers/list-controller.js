66cd3fb8da94f18cfdb5227b742e97628263e1e0
berto <berto@q-interactiva.com>
2016-04-12 14:38:33 +0200

    no message

--- www/js/controllers/list-controller.js
+++ www/js/controllers/list-controller.js
@@ -1,8 +1,13 @@
-angular.module('ser').controller('PlaylistCtrl', ['$stateParams', function( $stateParams) {
+angular.module('ser').controller('PlaylistCtrl', ['$stateParams', 'Utils', function( $stateParams, Utils) {
   var self = this;
 
   self.clickBoton = function()
   {
+  	var avl = Utils.appTwitterAvailability();
+
+  	//var cad = Utils.dateToDDMMYYYY();
+
+  	alert("AVL: " + avl);
     //alert("click!");
     var ID = "LauRozRub";
     window.open("fb://profile/" + ID, '_system');
--- www/js/services/utils.js
+++ www/js/services/utils.js
@@ -11,9 +11,34 @@ angular.module('ser').service('Utils', ['Configuration', function(Configuration)
       	
       	return (dd[1]?dd:"0"+dd[0]) + "-" +(mm[1]?mm:"0"+mm[0]) + "-" + yyyy; // padding
 	}
+
+
+	this.appTwitterAvailability = function()
+	{
+		var scheme;
+		// Don't forget to add the cordova-plugin-device plugin for `device.platform`
+		if(ionic.Platform.isIOS() == true) 
+		{
+		    scheme = 'twitter://';
+		}
+		else if(ionic.Platform.isAndroid() == true)
+		{
+		    scheme = 'com.twitter.android';
+		}
+
+		appAvailability.check(
+		    scheme,       // URI Scheme or Package Name
+		    function() {  // Success callback
+		        alert(scheme + ' is available :)');
+		        return true;
+		    },
+		    function() {  // Error callback
+		        alert(scheme + ' is not available :(');
+		        return false;
+		    }
+		);
+	}
  
-
-
 }]);
 
 
