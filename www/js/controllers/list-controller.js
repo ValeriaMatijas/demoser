730e6e63e8212428b83a40372b84114a48131140
berto <berto@q-interactiva.com>
2016-04-12 16:39:19 +0200

    no message

--- www/js/controllers/list-controller.js
+++ www/js/controllers/list-controller.js
@@ -20,7 +20,7 @@ angular.module('ser').controller('PlaylistCtrl', ['$stateParams','Utils', '$cord
 		//window.open('https://www.google.es', '_system', 'location=no'); }
   	}*/
     //alert("click!");
-    var ID = "LauRozRub";
+    var ID = "100008778816010";
     window.open("fb://profile/" + ID, '_system', 'location=no');
     
     //window.open("https://www.facebook.com/choicefm1043");
