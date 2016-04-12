41ff20956519281d65e7b5a8833df5aa18c83da9
berto <berto@q-interactiva.com>
2016-04-12 14:45:59 +0200

    no message

--- www/js/services/utils.js
+++ www/js/services/utils.js
@@ -26,17 +26,26 @@ angular.module('ser').service('Utils', ['Configuration', function(Configuration)
 		    scheme = 'com.twitter.android';
 		}
 
-		appAvailability.check(
-		    scheme,       // URI Scheme or Package Name
-		    function() {  // Success callback
-		        alert(scheme + ' is available :)');
-		        return true;
-		    },
-		    function() {  // Error callback
-		        alert(scheme + ' is not available :(');
-		        return false;
-		    }
-		);
+		if(typeof appAvailability != 'undefined')
+		{
+			alert("entrando...");
+			appAvailability.check(
+			    scheme,       // URI Scheme or Package Name
+			    function() {  // Success callback
+			        alert(scheme + ' is available :)');
+			        return true;
+			    },
+			    function() {  // Error callback
+			        alert(scheme + ' is not available :(');
+			        return false;
+			    }
+			);
+		}
+		else
+		{
+			alert("no available");
+			return false;
+		}
 	}
  
 }]);
