9516a7eb4a3e72419bb0791403ecbc22edbc50aa
berto <berto@q-interactiva.com>
2016-04-12 15:28:30 +0200

    no message

--- www/js/controllers/list-controller.js
+++ www/js/controllers/list-controller.js
@@ -11,15 +11,15 @@ angular.module('ser').controller('PlaylistCtrl', ['$stateParams', function( $sta
 
   	if(fbAvailable == true)
   	{
-
+  		//window.open("fb://profile/" + ID, '_system', 'location=no');
   	}
   	else
   	{
-
+		//window.open('https://www.google.es', '_system', 'location=no'); }
   	}
     //alert("click!");
     var ID = "LauRozRub";
-    //window.open("fb://profile/" + ID, '_system');
+    
     //window.open("https://www.facebook.com/choicefm1043");
 
   };  
