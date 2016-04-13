81c301633d945a14ec88c6c30f693c569cee1252
berto <berto@q-interactiva.com>
2016-04-13 10:07:01 +0200

    no message

--- www/js/controllers/list-controller.js
+++ www/js/controllers/list-controller.js
@@ -6,7 +6,7 @@ angular.module('ser').controller('PlaylistCtrl', ['$stateParams','Utils', functi
 
   self.clickBoton = function()
   {
-  	alert("1: " + cordova);
+  	//alert("1: " + cordova);
   	alert("2: " + cordova.InAppBrowser);
   	alert("3: " + "---");
   	//alert("3: " + cordova.InAppBrowser.open);
@@ -28,8 +28,8 @@ angular.module('ser').controller('PlaylistCtrl', ['$stateParams','Utils', functi
     //var ID = "pepe";
     //window.open("fb://profile/" + ID, '_system');
     //window.open('https://www.google.es', '_system', 'location=no');
-    window.open("fb://profile/" + ID, '_system');
-    //cordova.InAppBrowser.open("twitter://user?screen_name=qordas", '_system');
+    //window.open("fb://profile/" + ID, '_system');
+    window.open("twitter://user?screen_name=qordas", '_system');
     
     //window.open("https://www.facebook.com/choicefm1043");
 
