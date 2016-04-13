2d2aa80852adff1698964e8e15b2e02241bfe60f
berto <berto@q-interactiva.com>
2016-04-13 12:11:33 +0200

    no message

---
+++ www/js/controllers/home-controller.js
@@ -0,0 +1,13 @@
+angular.module('ser').controller('HomeCtrl', ['$stateParams','Utils', function( $stateParams, Utils) {
+  var self = this;
+
+
+  self.init = function()
+  {
+    console.log("init home");
+  	
+
+  };  
+
+  self.init();
+}]);
